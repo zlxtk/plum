713850543824e88c4f733257cc677499fafc0a4a
zlxtk <zlxtk@126.com>
2018-08-15 21:53:47 +0800

    redis

--- src/main/java/com/zlxtk/boot/plum/PlumApplication.java
+++ src/main/java/com/zlxtk/boot/plum/PlumApplication.java
@@ -2,8 +2,10 @@ package com.zlxtk.boot.plum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PlumApplication {
 
     public static void main(String[] args) {
