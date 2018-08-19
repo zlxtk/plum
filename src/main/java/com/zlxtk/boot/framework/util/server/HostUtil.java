0189bf41684cf7f4672095a11c884105ca393162
zlxtk <zlxtk@126.com>
2018-08-19 12:45:03 +0800

    2018年8月19日12:42:51

---
+++ src/main/java/com/zlxtk/boot/framework/util/server/HostUtil.java
@@ -0,0 +1,110 @@
+/*
+ * 版权所有 © 北京晟壁科技有限公司 2008-2027。保留一切权利!
+ */
+package com.zlxtk.boot.framework.util.server;
+
+import com.zlxtk.boot.framework.config.EmbeddedServletConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+/**
+ * 用途：主机工具栏
+ * 作者: lishuyi
+ * 时间: 2018/5/12  15:53
+ */
+@Component
+@DependsOn(value = {"embeddedServletConfiguration"})
+public class HostUtil {
+
+    @Autowired
+    private ServletWebServerApplicationContext server;
+
+    private static HostUtil hostUtil;
+
+    @PostConstruct
+    public void init() {
+        hostUtil = this;
+    }
+
+    public static boolean isWindowsOS() {
+        boolean isWindowsOS = false;
+        String osName = System.getProperty("os.name");
+        if (osName.toLowerCase().indexOf("windows") > -1) {
+            isWindowsOS = true;
+        }
+        return isWindowsOS;
+    }
+
+    public static String getHostName() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            return InetAddress.getLoopbackAddress().getHostName();
+        }
+    }
+
+    public static String getHostAddress() {
+        String sIP = "";
+        InetAddress ip = null;
+        try {
+            // 如果是Windows操作系统
+            if (isWindowsOS()) {
+                ip = InetAddress.getLocalHost();
+            }
+            // 如果是Linux操作系统
+            else {
+                boolean bFindIP = false;
+                Enumeration<NetworkInterface> netInterfaces = (Enumeration<NetworkInterface>) NetworkInterface
+                        .getNetworkInterfaces();
+                while (netInterfaces.hasMoreElements()) {
+                    if (bFindIP) {
+                        break;
+                    }
+                    NetworkInterface ni = (NetworkInterface) netInterfaces
+                            .nextElement();
+                    // ----------特定情况，可以考虑用ni.getName判断
+                    // 遍历所有ip
+                    Enumeration<InetAddress> ips = ni.getInetAddresses();
+                    while (ips.hasMoreElements()) {
+                        ip = (InetAddress) ips.nextElement();
+                        if ((ip.getHostAddress().endsWith(".0")) || (ip.getHostAddress().endsWith(".1"))) {
+                            continue;
+                        }
+                        if (ip.isSiteLocalAddress() && !ip.isLoopbackAddress() // 127.开头的都是lookback地址
+                                && ip.getHostAddress().indexOf(":") == -1) {
+                            bFindIP = true;
+                            break;
+                        }
+                    }
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (null != ip) {
+            sIP = ip.getHostAddress();
+        }
+        return sIP;
+    }
+
+    /**
+     * 获取服务器ip和端口信息
+     * 参考：http://ruitao.name/blog/20160111/tomcat-port/
+     *
+     * @return
+     */
+    public int getRunningPort() {
+        //return server.getWebServer().getPort();
+        return EmbeddedServletConfiguration.serverPort;
+    }
+}
