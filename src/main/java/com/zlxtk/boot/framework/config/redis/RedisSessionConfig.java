a9847efd15272f81ce0376442dd81bf75e9c885a
zlxtk <zlxtk@126.com>
2018-08-27 19:01:22 +0800

    2018年8月27日 19:01:17

--- src/main/java/com/zlxtk/boot/framework/config/redis/RedisSessionConfig.java
+++ src/main/java/com/zlxtk/boot/framework/config/redis/RedisSessionConfig.java
@@ -12,6 +12,10 @@ import org.springframework.session.web.http.DefaultCookieSerializer;
  * 参考：https://docs.spring.io/spring-data/redis/docs/2.0.1.RELEASE/reference/html/#new-in-2.0.0
  * 作者: zlxtk
  * 时间: 2018/8/18
+ *
+ * session中的数据：
+ * SPRING_SECURITY_CONTEXT-------org.springframework.security.core.context.SecurityContextImpl@b4418cf8: Authentication: org.springframework.security.authentication.UsernamePasswordAuthenticationToken@b4418cf8: Principal: SysUser(id=1, username=admin, truename=管理员, nickname=管理员, password=123456, email=null, sex=null, phone=null, roles=[SysRole(id=1, roleCode=ROLE_ADMIN, roleName=管理员, users=null, permissions=null), SysRole(id=2, roleCode=ROLE_USER, roleName=用户, users=null, permissions=null)]); Credentials: [PROTECTED]; Authenticated: true; Details: org.springframework.security.web.authentication.WebAuthenticationDetails@ffff4c9c: RemoteIpAddress: 0:0:0:0:0:0:0:1; SessionId: 138492aa-3c91-45b0-9eac-326286ed5c57; Granted Authorities: ROLE_USER, ROLE_ADMIN
+ * SPRING_SECURITY_SAVED_REQUEST-------DefaultSavedRequest[http://localhost:8080/plum/]
  */
 @Slf4j
 @Configuration
